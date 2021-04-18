adf1a5acc27cca49b80574ff083e3245026257b4
nada mohamed <nmmoharran@gmail.com>
2021-04-18 21:55:01 +0200

    the first part

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct student {
+char name [50];
+int id;
+int birthday[3];
+int score;};
+int main()
+{
+
+   printf("Welcome to our repo\n");
+   printf("please complete the following informations.\n ");
+    return 0;
+}
